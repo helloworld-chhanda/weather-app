ba8a5eb015f73353e13f2ad2cc585ad69571c790
Chhanda Naskar <chhandanaskar.official@gmail.com>
2023-04-06 22:43:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [value,setValue] = useState(0);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+const handleSearch=(e)=>{
+  setValue(e.target.value) 
+}
+useEffect(() => {
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=10&appid=9da2b0b83848d461537e697109a3e9ae`)
+  .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          const data = result[0]
+          const latitude = data.lat
+          const longitude = data.lon
+          fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=9da2b0b83848d461537e697109a3e9ae`)
+        })
+},[value])
+console.log(value)
+
+  // function increment()
+  // {
+  //   setValue(value+1)
+  
+  // }
+  // function decrement()
+  // { 
+  //   setValue(value-1)
+  // }
+  const [search,setSearch] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Weather-app">
+      <div>
+        <input type="text" onChange={handleSearch} name="city" placeholder='Enter city name' value={value} />
+        <button className="search" value="submit" >Search</button>
+        {/*<button onClick={increment}>+</button>*/}
+        <div className="temp">
+            
+        </div>
+        {/*<button onClick={decrement}>-</button>*/}
+      </div>
     </div>
   );
 }
